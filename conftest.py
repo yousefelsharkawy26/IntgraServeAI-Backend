da43b82add62d9e1afa054e3c6a8f5170804d1bc
Yousef Hani <hanyy4870@gmail.com>
2026-04-16 16:36:21 +0200

    Start Make Service And Api To Agent Config

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
+# conftest.py
 import os
 import pytest
 
-# نقوم بحقن متغيرات بيئة وهمية قبل أن يتم استيراد ملف الإعدادات الخاص بمشروعك
 os.environ["POSTGRES_USER"] = "test_user"
 os.environ["POSTGRES_PASSWORD"] = "test_password"
 os.environ["POSTGRES_DB"] = "test_db"
@@ -9,5 +9,3 @@ os.environ["SECRET_KEY"] = "super-secret-test-key-for-testing-only-12345"
 os.environ["SMTP_USER"] = "test_smtp"
 os.environ["SMTP_PASSWORD"] = "test_smtp_pass"
 os.environ["SMTP_FROM_EMAIL"] = "test@example.com"
-
-# إذا كان لديك أي متغيرات أخرى إجبارية في Settings، قم بإضافتها هنا بنفس الطريقة
