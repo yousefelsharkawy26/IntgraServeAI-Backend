2c418794142f523ee34217feeaf44c0304cffeee
yousefelsharkawy26 <yousefelsharkawy@std.mans.edu.eg>
2026-07-12 22:46:47 +0300

    update cors origin to all

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ app = FastAPI(
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ["http://localhost:3000", "http://localhost:8000", "*"],
+    allow_origins= ["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
